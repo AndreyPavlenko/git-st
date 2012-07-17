9e4c329b00f07cfd8c75af0cdb4c23a5fb623943
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2012-07-18 01:11:46 +0400

    Stub for push

---
+++ src/com/googlecode/gitst/Push.java
@@ -0,0 +1,11 @@
+package com.googlecode.gitst;
+
+/**
+ * @author Andrey Pavlenko
+ */
+public class Push {
+
+    public static void main(final String[] args) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
